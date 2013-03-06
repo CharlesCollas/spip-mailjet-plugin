def2dd72fdc5c319ba8b7ea949179711acc12906
Jeremy <jeremy@mailjet.com>
2013-03-06 18:35:18 +0200

    First Commit

---
+++ mailjet/lang/paquet-mailjet_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-mailjet?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'mailjet_description' => 'Allows you send your emails with Mailjet, as well as manage your lists',
+	'mailjet_nom' => 'Mailjet',
+	'mailjet_slogan' => 'Send your emails throught your mailjet account'
+);
+
+?>
---
+++ mailjet/lang/paquet-mailjet_fr.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-mailjet?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'mailjet_description' => 'Permet d\'envoyer tous vos emails à travers Mailjet, ainsi que de gérer vos contacts depuis l\'interface d\'aministration de Spip',
+	'mailjet_nom' => 'Mailjet',
+	'mailjet_slogan' => 'Envoyer vos courrier par votre compte Mailjet'
+);
+
+?>
